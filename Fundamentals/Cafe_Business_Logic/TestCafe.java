12b6ba22f658b1ce6b3ead57222ae548ba2d9546
Nikhil Virparia <nvirparia@gmail.com>
2021-09-27 21:33:45 -0400

    Cafe Business Logic

---
+++ Fundamentals/Cafe_Business_Logic/TestCafe.java
@@ -0,0 +1,16 @@
+public class TestCafe {
+
+    public static void main(String[] args) {
+        // TO-DO:
+        // Create an instance of the CafeUtil class
+        // in order to use the CafeUtil class' methods.
+
+        // Hint: it will need to correspond with the variable name
+        // used in your test code.
+
+        // Given Test Cases Here
+
+        System.out.println("Hello");
+
+    }
+}
