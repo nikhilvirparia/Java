42c977c87a17b3130cc539b8d6307b2123c636ef
Nikhil Virparia <nvirparia@gmail.com>
2021-09-27 21:16:59 -0400

    Cafe Business Logic

---
+++ Fundamentals/Cafe_Business_Logic/TestCafe.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class TestCafe {
+
+    public static void main(String[] args) {
+        // TO-DO:
+        // Create an instance of the CafeUtil class
+        // in order to use the CafeUtil class' methods.
+
+        // Hint: it will need to correspond with the variable name
+        // used in your test code.
+
+        // Given Test Cases Here
+
+        System.out.println("Hello");
+
+    }
+}
