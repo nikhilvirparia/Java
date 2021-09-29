0e1c3d6fdc5a9b467e20ae6b09492eb6524371e5
Nikhil Virparia <nvirparia@gmail.com>
2021-09-29 00:37:17 -0400

    Alfred Bot

---
+++ Fundamentals/alfredBot/AlfredQuotes.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class AlfredQuotes {
+
+    public String basicGreeting() {
+        // You do not need to code here, this is an example method
+        return "Hello, lovely to see you. How are you?";
+    }
+
+    public String guestGreeting(String name) {
+        // YOUR CODE HERE
+       System.out.println("Hello, " + name + ". Lovely to see you.");
+
+            return name;
+//        return "place holder for guest greeting return string";
+    }
+
+    public String dateAnnouncement() {
+        // YOUR CODE HERE
+        //return "place holder for date announcement return string";
+        Date date = new Date();
+        return "Current date is: " + date;
+    }
+
+    public String respondBeforeAlexis(String conversation) {
+        // YOUR CODE HERE
+        return "for snarky response return string";
+    }
+
+    // NINJA BONUS
+    // See the specs to overload the guessGreeting method
+    // SENSEI BONUS
+    // Write your own AlfredQuote method using any of the String methods you have learned!
+}
+
