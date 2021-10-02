9ceb6483d4b560ef4008cd68a73dd08c41f19a79
Nikhil Virparia <nvirparia@gmail.com>
2021-10-01 23:41:30 -0400

    Daikichi Routes updates

--- Spring/src/main/java/com/nikhil/daikichi/controllers/DaikichiControllers.java
+++ Spring/src/main/java/com/nikhil/daikichi/controllers/DaikichiControllers.java
@@ -1,5 +1,6 @@
 package com.nikhil.daikichi.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,20 @@ public class DaikichiControllers {
     @RequestMapping(value="/daikichi/tomorrow")
     public String tomorrow(){
         return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!  ";
+    }
+
+    @RequestMapping("/daikichi/travel/{city}")
+    public String showCity(@PathVariable("city") String city){
+        return "Congratulations! You will soon travel to "+ city +"!" ;
+    }
+
+    @RequestMapping("/daikichi/lotto/{number}")
+    public String showNumber(@PathVariable("number") int number){
+        if (number %2 == 1) {
+            return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+        } else {
+            return "You will take a grand journey in the near future, but be weary of tempting offers";
+        }
     }
 
 
